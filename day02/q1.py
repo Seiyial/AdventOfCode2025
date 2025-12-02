0bab74645a7e84e0161c85f31061693ce4002bc4
Sayhao Lim <sayhao@alpinevalleysoftware.co>
2025-12-03 02:09:13 +0800

    day2

---
+++ day02/q1.py
@@ -0,0 +1,13 @@
+def solve_02_1(input_data: str) -> str:
+	nums = set()
+	for row in input_data.split(","):
+		start, end = tuple([ int(val) for val in row.split("-") ])
+		for num in range(start, end + 1):
+			nums.add(num) if symmetrical(num) else None
+	return sum(nums)
+
+def symmetrical(num: int):
+	s = str(num)
+	length = len(s)
+	halflength = int(length / 2)
+	return length % 2 == 0 and s[0:halflength] == s[halflength:length]
---
+++ day02/q2.py
@@ -0,0 +1,31 @@
+def solve_02_2(input_data: str) -> None:
+	nums = set()
+	for row in input_data.split(","):
+		start, end = tuple([ int(val) for val in row.split("-") ])
+		for num in range(start, end + 1):
+			nums.add(num) if repetitive(num) else None
+	return sum(nums)
+
+def repetitive(num: int):
+	s = str(num)
+	length = len(s)
+	
+	for i in range(1, int(length / 2) + 1):
+
+		if length % i != 0:
+			continue
+
+		first = s[0:i]
+		ok = True
+
+		for j in range(1, int(length / i)):
+			cur = s[(j * i):((j + 1) * i)]
+			ok = cur == first
+			if not ok:
+				break
+
+		if ok:
+			return True
+		
+	return False
+
